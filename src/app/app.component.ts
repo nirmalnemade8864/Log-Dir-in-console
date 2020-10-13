0c6d38b979508b487ac9d59e07e46c110c758410
Nirmal@6798 <nirmalnemade8864@gmail.com>
2020-10-13 14:14:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Page2';
+  newPost = 'NO CONTENT';
+
+  onAddPost(postInput: HTMLTextAreaElement) {
+    // console.log(postInput);
+    console.dir(postInput);
+  }
 }
